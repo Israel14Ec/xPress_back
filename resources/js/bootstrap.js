f39145236c4f0def481e7979a22fc9784d5c2446
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 01:03:31 +0200

    boostrap configurado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,35 +1,27 @@
-import _ from 'lodash';
-window._ = _;
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+window.Pusher = Pusher;
 
-import axios from 'axios';
-window.axios = axios;
+const token = localStorage.getItem('AUTH_TOKEN');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const echoConfig = {
+    broadcaster: 'pusher',
+    key: 'miAppKeyProd', // Puede ser cualquier string ya que no estamos usando Pusher
+    wsHost: 'xpressback-production.up.railway.app', // Apunta a tu dominio de WebSockets
+    wsPort: 6001, // Puerto del servidor WebSockets
+    forceTLS: false, // Asegúrate de que esté en false si no estás usando SSL
+    disableStats: true,
+    enabledTransports: ['ws'], // Solo habilitar WebSocket sin SSL
+    cluster: 'mt1', // Asegúrate de incluir el cluster
+    authEndpoint: 'https://xpressback-production.up.railway.app/broadcasting/auth', // Cambia a tu endpoint de autenticación
+    auth: {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    }
+};
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+window.Echo = new Echo(echoConfig);
 
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+export default window.Echo;
