3ee105062a150a3213377652a8b98bf0d621560f
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 03:24:20 +0200

    Configuracion

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -8,16 +8,35 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => "df03ae27a8fe092e5efa",
-            'secret' => "3038071db340c273c723",
-            'app_id' => "1834186",
+            'key' => env('PUSHER_APP_KEY', 'df03ae27a8fe092e5efa'),
+            'secret' => env('PUSHER_APP_SECRET', '3038071db340c273c723'),
+            'app_id' => env('PUSHER_APP_ID', '1834186'),
             'options' => [
-                'cluster' => 'mt1',
-                'useTLS' => false, // O false si no estás usando SSL
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
-                'scheme' => 'http',
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'useTLS' => true,
+                'encrypted' => true,
+                // No configurar host, port y scheme para Pusher en producción
             ],
         ],
+
+        'ably' => [
+            'driver' => 'ably',
+            'key' => env('ABLY_KEY'),
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
     ],
+
 ];
