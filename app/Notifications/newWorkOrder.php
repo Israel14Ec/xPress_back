a20d44ac6fb730e3c0570b602aa75ff6ad24460b
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 05:13:29 +0200

    new work order

--- app/Notifications/newWorkOrder.php
+++ app/Notifications/newWorkOrder.php
@@ -4,76 +4,62 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
 
-class newWorkOrder extends Notification 
+class NewWorkOrder extends Notification
 {
     use Queueable;
 
-    public $idWorkOrder;
-    public $toUser;
+    public $workOrderId;
+    public $userId;
     public $subject;
-    public $jobTitle;
-    public $jobDescription;
-    public $workInstructions;
-    public $date;
+    public $jobName;
+    public $description;
+    public $instructions;
+    public $assignedDate;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct($idWorkOrder, $toUser, $subject, $jobTitle, $jobDescription, $workInstructions, $date)
+    public function __construct($workOrderId, $userId, $subject, $jobName, $description, $instructions, $assignedDate)
     {
-        $this->idWorkOrder = $idWorkOrder;
-        $this->toUser = $toUser;
+        $this->workOrderId = $workOrderId;
+        $this->userId = $userId;
         $this->subject = $subject;
-        $this->jobTitle = $jobTitle;
-        $this->jobDescription = $jobDescription;
-        $this->workInstructions= $workInstructions;
-        $this->date= $date;
+        $this->jobName = $jobName;
+        $this->description = $description;
+        $this->instructions = $instructions;
+        $this->assignedDate = $assignedDate;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
-        return ['database', 'broadcast'];
+        return ['broadcast', 'database'];
     }
 
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'data' => [
+                'id_work_order' => $this->workOrderId,
+                'to_user' => $this->userId,
+                'subject' => $this->subject,
+                'title' => $this->jobName,
+                'description' => $this->description,
+                'instructions' => $this->instructions,
+                'date' => $this->assignedDate,
+            ]
+        ]);
+    }
 
     public function toArray($notifiable)
     {
         return [
-            'id_work_order' => $this->idWorkOrder,
-            'to_user' => $this->toUser,
+            'id_work_order' => $this->workOrderId,
+            'to_user' => $this->userId,
             'subject' => $this->subject,
-            'title' => $this->jobTitle,
-            'description' => $this->jobDescription,
-            'instructions' => $this->workInstructions,
-            'date' =>  $this->date
+            'title' => $this->jobName,
+            'description' => $this->description,
+            'instructions' => $this->instructions,
+            'date' => $this->assignedDate,
         ];
     }
-
-    /*
-    public function broadcastWith()
-    {
-        return [
-            'id_work_order' => $this->idWorkOrder,
-            'to_user' => $this->toUser,
-            'title' => $this->jobTitle,
-            'description' => $this->jobDescription,
-            'instructions' => $this->workInstructions,
-            'date' => $this->date,
-        ];
-    }*/
-
-
 }
