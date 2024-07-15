6af46c6267ecbe3c1c5204c86e1ea459f5381857
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 03:32:04 +0200

    Configuracion

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -8,11 +8,11 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY', 'df03ae27a8fe092e5efa'),
-            'secret' => env('PUSHER_APP_SECRET', '3038071db340c273c723'),
-            'app_id' => env('PUSHER_APP_ID', '1834186'),
+            'key' => "df03ae27a8fe092e5efa",
+            'secret' => "3038071db340c273c723",
+            'app_id' => "1834186",
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'cluster' => "mt1",
                 'useTLS' => true,
                 'encrypted' => true,
                 // No configurar host, port y scheme para Pusher en producción
