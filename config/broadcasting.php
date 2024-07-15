e2fbeebae9dcb029a94de5700ac00b46eea0b7e5
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 03:16:34 +0200

    Configuracion

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -2,42 +2,22 @@
 
 return [
 
-    'default' => 'pusher',
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
 
     'connections' => [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => 'df03ae27a8fe092e5efa',
-            'secret' => '3038071db340c273c723',
-            'app_id' => '1834186',
+            'key' => "df03ae27a8fe092e5efa",
+            'secret' => "3038071db340c273c723",
+            'app_id' => "1834186",
             'options' => [
                 'cluster' => 'mt1',
-                'useTLS' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
+                'useTLS' => false, // O false si no estás usando SSL
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
                 'scheme' => 'http',
             ],
         ],
-
-        'ably' => [
-            'driver' => 'ably',
-            'key' => env('ABLY_KEY'),
-        ],
-
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-        ],
-
-        'log' => [
-            'driver' => 'log',
-        ],
-
-        'null' => [
-            'driver' => 'null',
-        ],
-
     ],
-
 ];
